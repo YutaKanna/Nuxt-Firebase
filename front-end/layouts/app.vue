a2bcb2d08042158ed82081ae056d03d5b74db590
YutaKanna <kannayuta0629@gmail.com>
2020-08-16 19:18:29 +0900

    一覧ページ(静的)を作成する #5

---
+++ front-end/layouts/app.vue
@@ -0,0 +1,19 @@
+<template>
+<body>
+  <div>
+    <section class="hero is-primary is-bold">
+        <div class="hero-body">
+        <h1 class="title is-size-2 has-text-centered">
+            something
+        </h1>
+        <h2 class="subtitle is-size-4 has-text-centered">
+            something
+        </h2>
+        </div>
+    </section>
+    <div class="container">
+        <nuxt/>
+    </div>
+  </div>
+</body>
+</template>
