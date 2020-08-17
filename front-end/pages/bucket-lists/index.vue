aeeda8e10bf252cda5c37b852a06cbef249b0067
YutaKanna <kannayuta0629@gmail.com>
2020-08-17 15:17:40 +0900

    wip

--- front-end/pages/bucket-lists/index.vue
+++ front-end/pages/bucket-lists/index.vue
@@ -1,37 +1,11 @@
 <template>
-    <div class="container">
-        <div class="notification">
-            This container is <strong>centered</strong> on desktop.
-        </div>
-        <div class="culumns">
-            <template v-for="bucketList in bucketLists">
-                <div class="column">
-                    <div class="card" :key="bucketList.id">
-                        <div class="card-content">
-                            <p class="title">
-                            “{{ bucketList.title }}”
-                            </p>
-                            <p class="subtitle">
-                            Jeff Atwood
-                            </p>
-                        </div>
-                        <footer class="card-footer">
-                            <p class="card-footer-item">
-                            <span>
-                                View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
-                            </span>
-                            </p>
-                            <p class="card-footer-item">
-                            <span>
-                                Share on <a href="#">Facebook</a>
-                            </span>
-                            </p>
-                        </footer>
-                    </div>
-                </div>
-            </template>
-        </div>
-    </div>
+    <ul>
+        <li v-for="bucketList in bucketLists" :key="bucketList.id">
+            <h4>
+                <span>{{bucketList.title}}</span>
+            </h4>
+        </li>
+    </ul>
 </template>
 
 <script>
@@ -40,7 +14,7 @@ import axios from 'axios'
 export default {
   layout: 'app',
   async asyncData({ $axios }) {
-    const response = await $axios.$get("http://localhost:8000/bucket-lists")
+    const response = await $axios.$get("http://localhost:8000/api/bucket-lists")
         .catch( error => {
         console.log("response error", error)
         return false
