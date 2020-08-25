a6ada4dbd641ff8c665fe1a53e47b183b0536175
YutaKanna <kannayuta0629@gmail.com>
2020-08-25 19:03:57 +0900

    投稿画面を作成する #6

--- nuxt/pages/bucket-lists/new.vue
+++ nuxt/pages/bucket-lists/new.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    <section class="container">
-        <div>hoge</div>
+    <section>
+        <b-field label="Title">
+            <b-input value=""></b-input>
+        </b-field>
+
+        <b-field label="Discription">
+            <b-input value=""></b-input>
+        </b-field>
+
+        <b-field>
+            <p class="control">
+                <button class="button is-primary">
+                  Post
+                </button>
+            </p>
+        </b-field>
     </section>
-  </div>
 </template>
 
 <script>
 import axios from 'axios';
 export default {
+    layout: 'app',
 }
 </script>
