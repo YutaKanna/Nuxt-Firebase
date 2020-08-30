b1a9e20f669e360da2dd124c6d7e093bac405f3e
YutaKanna <kannayuta0629@gmail.com>
2020-08-30 18:16:44 +0900

    投稿時にアラートを出すようにする #6

--- nuxt/pages/bucket-lists/new.vue
+++ nuxt/pages/bucket-lists/new.vue
@@ -1,44 +1,36 @@
 <template>
-    <section>
-        <form
-            id="app"
-            @submit="checkForm"
-            action="http://localhost:8000/api/bucket-lists"
-            method="post"
-        >
-            <b-field label="Title">
-                <b-input name="title"></b-input>
-            </b-field>
+    <form>
+        <b-field label="Title">
+            <b-input name="title"></b-input>
+        </b-field>
 
-            <b-field label="Description">
-                <b-input name="description"></b-input>
-            </b-field>
+        <b-field label="Description">
+            <b-input name="description"></b-input>
+        </b-field>
 
-            <b-field>
-                <p class="control">
-                    <button class="button is-primary">
-                    Post
-                    </button>
-                </p>
-            </b-field>
-        </form>
-    </section>
+        <b-field>
+            <p class="control">
+                <button @click="addData">Click</button>
+            </p>
+        </b-field>
+    </form>
 </template>
+
 
 <script>
 import axios from 'axios';
+let url = "http://localhost:8000/api/bucket-lists";
 export default {
-    layout: 'app',
-    data () {
+    data: function(){
         return {
-        email: '',
-        password: ''
-        }
+            title:'',
+            description:'',
+        };
     },
     methods: {
-        goBack () {
-            this.$router.push('/')
-        }
-    }
+        addData: function() {
+            alert("hello world!");
+        },
+    },
 }
 </script>
