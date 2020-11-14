43217e542ddbe65047cf16538f09e1921e0df5c5
YutaKanna <kannayuta0629@gmail.com>
2020-11-14 19:40:38 +0900

    wip postできるけど、登録がされていない #21

---
+++ nuxt/pages/types/new.vue
@@ -0,0 +1,26 @@
+<template>
+  <form method="post">
+    <input type="text" name="name" v-model="name" />
+    <button type="submit" @click="onSubmit">submit</button>
+  </form>
+</template>
+
+
+<script>
+import firebase from "~/plugins/firebase.js"
+import 'firebase/firestore'
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      var db = firebase.firestore()
+      db.collection('types').doc('hoge').set({
+        name: this.name
+      })
+    }
+  }
+}
