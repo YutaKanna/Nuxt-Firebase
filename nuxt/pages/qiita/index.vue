2ff121fb32ec3f60db6954f59aa7f194a4e0b020
YutaKanna <kannayuta0629@gmail.com>
2020-12-26 23:42:24 +0900

    自分が投稿した記事の一覧を表示 #23

--- nuxt/pages/qiita/index.vue
+++ nuxt/pages/qiita/index.vue
@@ -12,18 +12,13 @@
 import axios from 'axios';
 export default {
   layout: 'app',
-  head() {
-    return {
-      meta: [
-        {
-          Authorization: 'Bearer f83525680591a1843c31d71019528acb9d8a1d9b'
-        }
-      ]
-    }
-  },
   async asyncData({ app }) {
-    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js');
-    console.log(items);
+    const items = await app.$axios.$get('https://qiita.com/api/v2/authenticated_user/items', {
+        headers: {'Content-Type': 'application/json', 'Authorization': process.env.QIITA_TOKEN},
+        params: {
+            page: 1
+        }
+    });
     return {
       items
     }
