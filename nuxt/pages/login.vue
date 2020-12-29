ee228f57da37d0b9b1c3b2bffcd0575c43212d09
YutaKanna <kannayuta0629@gmail.com>
2020-12-29 22:00:38 +0900

    Firebase Authenticationを使って、新規登録、ログインできるようにする #28

---
+++ nuxt/pages/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>ログイン</div>
+    <form @submit.prevent="submit">
+      <input type="email" name="email" size="25" v-model="email"/>
+      <input type="password" name="password" size="25" v-model="password"/>
+      <button
+       class="button"
+       type="submit"
+       @click="submit"
+      >submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import firebase from "~/plugins/firebase.js"
+import 'firebase/firestore'
+import 'firebase/auth';
+export default {
+    data: function() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      submit() {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+        .then(this.$router.push('/'));
+      }
+    }
+}
+</script>
